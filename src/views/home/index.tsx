a1dbe87fb3dc33c98285f54e0d3f107aba5f6fac
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-20 14:45:20 -0400

    feat: Add page home

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
 import * as S from './styles';
-import SideBar from "../../components/sidebar";
+
+import TopBar from "../../components/topbar";
 import Tasks from "../../components/tasks";
 
+import { IResponseTask } from '../../types/typesInterface';
+import { api } from '../../services/api';
+
 const Home = () => {
+  const [tasks, setTasks] = React.useState<IResponseTask[]>([]);
+  useEffect(() => {
+    api.getTasks().then((response) => {
+      setTasks(response.data);
+  })}, [])
+
+
   return(
     <S.HomeContainer>
-      <SideBar/>
-      <Tasks/>
+      <TopBar/>
+      <Tasks tasks={tasks}/>
     </S.HomeContainer>
   )
 }
--- src/views/home/styles.ts
+++ src/views/home/styles.ts
@@ -1,3 +1,5 @@
 import styled from "styled-components"
 
-export const HomeContainer = styled.section``
+export const HomeContainer = styled.section`
+  display: grid;
+`
