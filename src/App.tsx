3c70491ccbd3d4f8e7c9c20cb2afc47d499b3d75
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-21 19:53:04 -0400

    fix: Correction of component location

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import GlobalStyles from './styles/index';
 import { RoutesApp } from './routes/index';
 import { BrowserRouter } from 'react-router-dom';
-
+import Header from './components/header';
 const App=()=> {
   return (
     <BrowserRouter>
+      <Header/>
       <RoutesApp/>
       <GlobalStyles />
     </BrowserRouter>
