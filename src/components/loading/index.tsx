224029f5ed1c00345b84ce653c56d93becb6ed07
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-29 14:42:27 -0400

    feat: Create component loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as S from "./styles";
+export const Loading = () => {
+  return (
+      <S.Container>
+        <S.Loading >
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </S.Loading>
+      </S.Container>
+  );
+};
+
+export default Loading;
