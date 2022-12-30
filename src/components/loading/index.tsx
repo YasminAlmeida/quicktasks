fcb0e4d15f72803d21e2e150a5f65a680b6f79fd
YasminAlmeida <yasmin.almeidavd@gmail.com>
2022-12-30 13:58:47 -0400

    fix: Component modification

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,35 +1,11 @@
 import React from "react";
+import { PropagateLoader } from "react-spinners";
 import * as S from "./styles";
 export const Loading = () => {
   return (
-      <S.Container>
-        <S.Loading >
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-        </S.Loading>
-      </S.Container>
+    <S.Container>
+      <PropagateLoader color="#333" />
+    </S.Container>    
   );
 };
 
